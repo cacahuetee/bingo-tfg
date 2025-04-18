18ce4eeb504dfb7456cb4ef1e5dd7b1e127309a4
cacahuetee <elsa.ferreras.patonn@gmail.com>
2025-04-18 21:20:15 +0200

    Cambios para realizar con gunicorn para produccion

--- app.py
+++ app.py
@@ -230,3 +230,4 @@ def manejar_generar_numero(data):
 # Ejecutar la app (Render friendly)
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
